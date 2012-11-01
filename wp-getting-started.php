6a591cbb19182b2401ad4eea4e7aac92a734aea6
mike <mike@TrenvOS>
2012-11-01 10:50:07 +0200

    Added instruction panel

--- wp-getting-started.php
+++ wp-getting-started.php
@@ -60,7 +60,7 @@ if ( ! class_exists('WPGettingStarted') ) :
         public function __construct() {
             remove_action( 'welcome_panel', 'wp_welcome_panel' );
             add_action   ( 'welcome_panel', array ( &$this, 'the_welcome_panel' ) );
-            add_action   ( 'wp_after_welcome_panel', array ( &$this, 'the_completed_panel' ) );
+            add_action   ( 'wp_after_welcome_panel', array ( &$this, 'the_instruction_panel' ) );
         }
 
             /**
@@ -101,19 +101,24 @@ if ( ! class_exists('WPGettingStarted') ) :
         }
 
 
-        public function the_completed_panel() {
+        public function the_instruction_panel() {
             ?>
             <div class="welcome-panel-column-container">
 
-            <div class="welcome-panel-column">
-                <h4><?php _e( 'Get Started' ); ?></h4>
-                <p><?php _e( 'First, tweak the look of your site:' ); ?></p>
-                <a class="button-primary welcome-button load-customize hide-if-no-customize" href="<?php echo wp_customize_url(); ?>"><?php _e( 'Customize Your Site' ); ?></a>
-                <a class="button-primary welcome-button hide-if-customize" href="<?php echo admin_url( 'themes.php' ); ?>"><?php _e( 'Customize Your Site' ); ?></a>
-                <?php if ( current_user_can( 'install_themes' ) || ( current_user_can( 'switch_themes' ) && count( wp_get_themes( array( 'allowed' => true ) ) ) > 1 ) ) : ?>
-                    <p class="hide-if-no-customize"><?php printf( __( 'or, <a href="%s">change your theme completely</a>' ), admin_url( 'themes.php' ) ); ?></p>
-                <?php endif; ?>
-            </div>
+            <?php if ( $this->complete < 2 ) : ?>
+
+                <div class="welcome-panel-column">
+                    <h4><?php _e( 'Congratulations!', 'wp-getting-started' ); ?></h4>
+                    <p><?php _e( 'Your new website is now up and running.', 'wp-getting-started' ); ?></p>
+                    <p><?php _e( 'To get started, follow the easy steps above, or', 'wp-getting-started' ); ?></p>
+
+                    <?php if ( current_user_can( 'install_themes' ) || ( current_user_can( 'switch_themes' ) && count( wp_get_themes( array( 'allowed' => true ) ) ) > 1 ) ) : ?>
+                        <a class="button-primary welcome-button" href="<?php echo admin_url( 'themes.php?live=1' ); ?>"><?php _e( 'Click here to get started' ); ?></a>
+                    <?php endif; ?>
+                </div>
+
+            <?php endif; ?>
+
             <div class="welcome-panel-column">
                 <h4><?php _e( 'Next Steps' ); ?></h4>
                 <ul>
@@ -169,6 +174,9 @@ if ( ! class_exists('WPGettingStarted') ) :
                 div.welcome-progression-block * {
                     text-align: center;
                 }
+                div.welcome-progression-block a p {
+                    width: 148px;
+                }
                 div.welcome-progression-block a, div.welcome-progression-block a p {
                     text-decoration: none;
                     color: #21759B;
@@ -201,12 +209,12 @@ if ( ! class_exists('WPGettingStarted') ) :
             <div class="welcome-panel-column-container">
 
                 <div class="welcome-progression-block">
-                    <h2>1. <?php _e( 'Website', 'wp-gettings-started' ); ?></h2>
+                    <h2>1. <?php _e( 'Website', 'wp-getting-started' ); ?></h2>
 
                     <a href="<?php bloginfo('url'); ?>">
                         <img src="<?php echo WPGS_IMAGES_URL . "setup"; ?>.png">
 
-                        <p class="completed"><?php _e( 'Setup your website', 'wp-gettings-started' ); ?></p>
+                        <p class="completed"><?php _e( 'Setup your website', 'wp-getting-started' ); ?></p>
                     </a>
                 </div>
 
@@ -214,19 +222,19 @@ if ( ! class_exists('WPGettingStarted') ) :
 
                 <div class="welcome-progression-block">
 
-                    <h2>2. <?php _e( 'Theme', 'wp-gettings-started' ); ?></h2>
+                    <h2>2. <?php _e( 'Theme', 'wp-getting-started' ); ?></h2>
                     <div class="welcome-progression-block">
 
                         <a href="<?php echo admin_url( 'themes.php' ) . '?live=1'; ?>">
                             <img src="<?php echo WPGS_IMAGES_URL . "change"; if ( ! $progress['theme_edited'] ) echo "_incomplete";  ?>.png">
 
-                            <p<?php if ( $progress['theme_edited'] ) echo " class='completed'"; ?>><?php _e ( 'Change', 'wp-gettings-started' ); ?></p>
+                            <p<?php if ( $progress['theme_edited'] ) echo " class='completed'"; ?>><?php _e ( 'Change', 'wp-getting-started' ); ?></p>
                         </a>
 
                     </div>
 
                     <div class="welcome-progression-block separate">
-                        <h2><?php _e( 'or', 'wp-gettings-started' ); ?></h2>
+                        <h2><?php _e( 'or', 'wp-getting-started' ); ?></h2>
                     </div>
 
                     <div class="welcome-progression-block">
@@ -241,23 +249,23 @@ if ( ! class_exists('WPGettingStarted') ) :
                 <?php $this->print_arrow ( 2 ); ?>
 
                 <div class="welcome-progression-block">
-                    <h2>3. <?php _e( 'Pages', 'wp-gettings-started' ); ?></h2>
+                    <h2>3. <?php _e( 'Pages', 'wp-getting-started' ); ?></h2>
 
                     <a href="<?php echo admin_url( 'post-new.php?post_type=page' ); ?>">
                         <img src="<?php echo WPGS_IMAGES_URL . "pages"; if ( ! $progress['has_pages'] ) echo "_incomplete";  ?>.png">
 
-                        <p<?php if ( $progress['has_pages'] ) echo " class='completed'"; ?>><?php _e( 'Add some pages to your website', 'wp-gettings-started' ); ?></p>
+                        <p<?php if ( $progress['has_pages'] ) echo " class='completed'"; ?>><?php _e( 'Add some pages to your website', 'wp-getting-started' ); ?></p>
                     </a>
                 </div>
 
                 <?php $this->print_arrow ( 3 ); ?>
 
                 <div class="welcome-progression-block">
-                    <h2>4. <?php _e ( 'Posts', 'wp-gettings-started' ); ?></h2>
+                    <h2>4. <?php _e ( 'Posts', 'wp-getting-started' ); ?></h2>
 
                     <a href="<?php echo admin_url( 'post-new.php' ); ?>">
                         <img src="<?php echo WPGS_IMAGES_URL . "posts"; if ( ! $progress['has_posts'] ) echo "_incomplete";  ?>.png">
-                        <p<?php if ( $progress['has_posts'] ) echo " class='completed'"; ?>><?php _e( 'Create your first blog entry','wp-gettings-started' ); ?></p>
+                        <p<?php if ( $progress['has_posts'] ) echo " class='completed'"; ?>><?php _e( 'Create your first blog entry','wp-getting-started' ); ?></p>
                     </a>
                 </div>
 
@@ -265,46 +273,6 @@ if ( ! class_exists('WPGettingStarted') ) :
 
             <?php do_action( 'wp_after_welcome_panel'); ?>
 
-            <?php // Left in the original ( WP3.5 ) welcome pane code if you want to include it ?>
-            <?php /*
-            <div class="welcome-panel-column-container">
-
-            <div class="welcome-panel-column">
-                <h4><?php _e( 'Get Started' ); ?></h4>
-                <p><?php _e( 'First, tweak the look of your site:' ); ?></p>
-                <a class="button-primary welcome-button load-customize hide-if-no-customize" href="<?php echo wp_customize_url(); ?>"><?php _e( 'Customize Your Site' ); ?></a>
-                <a class="button-primary welcome-button hide-if-customize" href="<?php echo admin_url( 'themes.php' ); ?>"><?php _e( 'Customize Your Site' ); ?></a>
-                <?php if ( current_user_can( 'install_themes' ) || ( current_user_can( 'switch_themes' ) && count( wp_get_themes( array( 'allowed' => true ) ) ) > 1 ) ) : ?>
-                    <p class="hide-if-no-customize"><?php printf( __( 'or, <a href="%s">change your theme completely</a>' ), admin_url( 'themes.php' ) ); ?></p>
-                <?php endif; ?>
-            </div>
-            <div class="welcome-panel-column">
-                <h4><?php _e( 'Next Steps' ); ?></h4>
-                <ul>
-                <?php if ( 'page' == get_option( 'show_on_front' ) && ! get_option( 'page_for_posts' ) ) : ?>
-                    <li><?php printf( '<a href="%s">' . __( 'Edit your front page' ) . '</a>', get_edit_post_link( get_option( 'page_on_front' ) ) ); ?></li>
-                    <li><?php printf( '<a href="%s">' . __( 'Add additional pages' ) . '</a>', admin_url( 'post-new.php?post_type=page' ) ); ?></li>
-                <?php elseif ( 'page' == get_option( 'show_on_front' ) ) : ?>
-                    <li><?php printf( '<a href="%s">' . __( 'Edit your front page' ) . '</a>', get_edit_post_link( get_option( 'page_on_front' ) ) ); ?></li>
-                    <li><?php printf( '<a href="%s">' . __( 'Add additional pages' ) . '</a>', admin_url( 'post-new.php?post_type=page' ) ); ?></li>
-                    <li><?php printf( '<a href="%s">' . __( 'Add a blog post' ) . '</a>', admin_url( 'post-new.php' ) ); ?></li>
-                <?php else : ?>
-                    <li><?php printf( '<a href="%s">' . __( 'Write your first blog post' ) . '</a>', admin_url( 'post-new.php' ) ); ?></li>
-                    <li><?php printf( '<a href="%s">' . __( 'Add an About page' ) . '</a>', admin_url( 'post-new.php?post_type=page' ) ); ?></li>
-                <?php endif; ?>
-                    <li><?php printf( '<a href="%s">' . __( 'View your site' ) . '</a>', home_url( '/' ) ); ?></li>
-                </ul>
-            </div>
-
-            <div class="welcome-panel-column welcome-panel-last">
-                <h4><?php _e( 'Learn How To' ); ?></h4>
-                <ul>
-                    <li><?php printf( '<a id="wp350_add_images" href="%s">' . __( 'Add image/media' ) . '</a>', admin_url( 'media-new.php' ) ); ?></li>
-                    <li><?php printf( '<a id="wp350_widgets" href="%s">' . __( 'Add/remove widgets' ) . '</a>', admin_url( 'widgets.php' ) ); ?></li>
-                    <li><?php printf( '<a id="wp350_edit_menu" href="%s">' . __( 'Edit your navigation menu' ) . '</a>', admin_url( 'nav-menus.php' ) ); ?></li>
-                </ul>
-            </div>
-             */ ?>
             </div>
             </div>
 
